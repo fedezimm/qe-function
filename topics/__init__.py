24afe316b79997f598a785683eaa2fe552eb2d4e
Federico Zimmermann <fedezimm@gmail.com>
2021-09-28 19:39:08 -0300

    adding changes

--- topics/__init__.py
+++ topics/__init__.py
@@ -3,9 +3,17 @@ import azure.functions as func
 from controller.topic import getTopic
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-  logging.info('GET /topics')
+  logging.info('POST /topics')
 
-  term = req.params.get('term')
+  #term = req.params.get('term')
+  
+  try:
+      req_body = req.get_json()
+  except ValueError:
+      pass
+  else:
+      term = req_body.get('query')
+
   if term:
     response = getTopic(term)
     return func.HttpResponse(f"{response}")
