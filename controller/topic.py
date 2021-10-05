8d21c130d7532eded79937e9553db4d9992b0cc1
Federico Zimmermann <fedezimm@gmail.com>
2021-10-05 00:53:56 -0300

    desambiguation

--- controller/topic.py
+++ controller/topic.py
@@ -2,15 +2,22 @@ from services.topic import getTopicsFromModel
 import json
 
 def getTopic(term):
-  topics = getTopicsFromModel(term)
+  topics, desambiguation_terms = getTopicsFromModel(term)
   response = {
     'key': [term],
     'topics': [{
       'id': topic[0],
       'keys': [{
-        key[0]: str(key[1])
-      } for key in topic[1]]
+        'word': topic[1][index][0], #character
+        'value': str(topic[1][index][1]), #0.001
+        'ds_term': desambiguation_terms[index] #character (novel)
+      } for index in range(len(topic[1]))]
     } for topic in topics]
   }
-
   return json.dumps(response)
+
+
+  #batman y superman
+  #['batman','superman']
+  #[(world,092,world), (character,097,[character(novel), character(film)]), new york, guason), (character, pelirroja, azul, rojo)]
+  #[0]
--- services/topic.py
+++ services/topic.py
@@ -1,6 +1,36 @@
+from wikipedia.exceptions import DisambiguationError
 from services.model import prepare_data_model, get_model
 
 def getTopicsFromModel(term):
   corpus, id2word, _, n_topics, _ = prepare_data_model('', [term])
   model = get_model(corpus, id2word, n_topics)
-  return model.show_topics(num_words=50, formatted=False)
+  topic = model.show_topics(num_words=50, formatted=False)
+  desambiguation_terms = getDesambiguationTerms(topic)
+
+  return topic, desambiguation_terms
+
+def getDesambiguationTerms(topic):
+  # Initialize a list
+  desambiguation_terms = []
+  # For each term in topic:
+  for term in topic:
+    # Store None value in variable X
+    desambiguation_term = term
+    
+    try:
+      # Search term in wikipedia (try)
+      pass
+    # If there is a DesambiguationError (except)
+    except:
+      value = 0
+      # Iterate throw each DesambiguationOption and extract from each the word in parenthesis
+      for ds_term in err.options:
+        # Apply function to that word that returns us a value of belongness to the topic
+        # Store the  index that is greater than every case
+        if actual_value > value: 
+          index = actual_index_value
+      # Store the best desambiguation term in variable X
+      desambiguation_term = err.options[index]
+    # Append variable X in the list (else)
+    desambiguation_terms.append(desambiguation_term)
+  return desambiguation_terms
